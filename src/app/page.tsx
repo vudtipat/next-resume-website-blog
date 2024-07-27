9695007bd7850646ab16a57ec7f5bbb7bacf4e34
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-07-27 15:33:34 +0700

    edit landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,31 @@
+'use client'
+import { TypeAnimation } from "react-type-animation";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p>root page1</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page</p>
-      <p>root page last</p>
+    <div className="flex flex-col h-[100%] justify-center items-center">
+      <TypeAnimation
+        sequence={[          
+          "",
+          1000,
+          "สวัสดี, I'm Vudtipat Saishana 👋🏻",
+          1000,
+          "Wellcome to my website (˶ˆᗜˆ˵)",
+          1000
+        ]}
+        speed={50}
+        style={{ fontSize: '30px', fontWeight: 'lighter' }}
+      />
+      <div className="flex gap-x-6 flex-row justify-between mt-[100px]">
+        <Link target="_blank" href={'https://github.com/vudtipat/'}>
+          <FaGithub size={36}/>
+        </Link>
+        <Link target="_blank" href={'https://www.linkedin.com/in/vudtipat-saichana-b65312132/'}>
+          <FaLinkedin size={36}/>
+        </Link>
+      </div>
     </div>
   );
 }
