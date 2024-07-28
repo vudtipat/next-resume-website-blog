6e862ec784daf1e02b081eaf5dc4219005a45da2
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-07-29 01:11:19 +0700

    refactor code for responsive support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-[100%] justify-center items-center">
+    <div className="flex px-3 flex-col h-[100%] justify-center items-center">
       <TypeAnimation
         sequence={[          
           "",
           1000,
           "สวัสดี, I'm Vudtipat Saishana 👋🏻",
           1000,
-          "Wellcome to my website (˶ˆᗜˆ˵)",
+          "Wellcome to my website (˶ˆᗜˆ˵) ...",
           1000
         ]}
         speed={50}
-        style={{ fontSize: '30px', fontWeight: 'lighter' }}
+        style={{wordBreak:'break-word'}}
+        className="text-xl text-center font-thin break-words lg:text-3xl lg:font-light"
       />
       <div className="flex gap-x-6 flex-row justify-between mt-[100px]">
         <Link target="_blank" href={'https://github.com/vudtipat/'}>
