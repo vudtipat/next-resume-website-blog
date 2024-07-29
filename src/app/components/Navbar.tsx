47d902ef4e36bc42ac7362d9416c8d2162c1b3a7
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-07-29 12:45:54 +0700

    refactor responsive menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,41 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 const Navbar = () => {
+
+    const [showMenu,setShowMenu] = useState(false);
+
     return (
-        <div className="py-[10px] px-[30px] flex items-center justify-between h-[10vh] border-b border-green-900/10">
-            <span className='py-[14px] px-[30px] text-[#333] no-underline font-[800] text-2xl'>VudtipatSai</span>        
-            <div className='flex font-extrabold'>                
-                <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/">Home</Link>
-                <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/about">About Me</Link>
-                <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/blog">Blog</Link>
-            </div>            
+        <div className='flex flex-col relative'>
+            <div className="py-[10px] px-0 md:px-[30px] flex items-center justify-between h-[10vh] border-b border-green-900/10">
+                <span className='py-[14px] px-4 md:px-[30px] text-[#333] no-underline font-[800] text-2xl'>VudtipatSai</span>        
+                <div className='hidden md:flex md:font-extrabold'>                
+                    <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/">Home</Link>
+                    <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/about">About Me</Link>
+                    <Link className='py-[14px] px-[30px] no-underline text-[#333] hover:text-[#A91D3A]' href="/blog">Blog</Link>
+                </div>   
+                <div className='px-4 flex items-center justify-center md:hidden'>
+                    {showMenu ?                        
+                        <GrClose size={30} onClick={() => setShowMenu(!showMenu)}/> :
+                        <GiHamburgerMenu size={30} onClick={() => setShowMenu(!showMenu)}/> 
+                    }
+                </div>         
+            </div>
+            {
+                showMenu ? (
+                    <div className='flex flex-col absolute w-[100%] top-[10vh] bg-white'>
+                        <Link className='py-[14px] px-4 no-underline text-[#333] font-extrabold border-b border-green-900/10' onClick={() => setShowMenu(!showMenu)} href="/">Home</Link>
+                        <Link className='py-[14px] px-4 no-underline text-[#333] font-extrabold border-b border-green-900/10' onClick={() => setShowMenu(!showMenu)} href="/about">About Me</Link>
+                        <Link className='py-[14px] px-4 no-underline text-[#333] font-extrabold border-b border-green-900/10' onClick={() => setShowMenu(!showMenu)} href="/blog">Blog</Link>
+                    </div>) : 
+                    <></>
+            }            
         </div>
+        
     );
 };
 
